85333a0511dc438f286ceb85d4ce422cdd0a8aaf
roverchen <rover.k.chen@gmail.com>
2022-07-30 22:46:49 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+import 'simplebar-react/dist/simplebar.min.css'
+import '@/stems/dist/avenir.css'
+import '@/stems/dist/stems.css'
+import './index.css'
+
+ReactDOM.render(<App />, document.getElementById('root'))
